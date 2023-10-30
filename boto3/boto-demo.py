891fed1d3adf131911f0a78ba272afabc6c8d494
EC2 Default User <ec2-user@ip-10-16-10-179.us-west-2.compute.internal>
2023-10-30 04:06:43 +0000

    Changing some naming convention of files.

--- boto3/boto-demo.py
+++ boto3/boto-demo.py
@@ -6,8 +6,3 @@ s3 = boto3.resource('s3')
 # Print out bucket names
 for bucket in s3.buckets.all():
     print(bucket.name)
-    
-# Crea un nuevo bucket
-s3.create_bucket(Bucket="test-boto3-d")
-
-print(f"Bucket '{bucket_name}' creado exitosamente en la región '{region}'.")
