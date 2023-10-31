06fc51cf8c2eff8de2a9dfe977d1b56b55ec0446
EC2 Default User <ec2-user@ip-10-16-10-179.us-west-2.compute.internal>
2023-10-31 01:59:35 +0000

    Some modifications.

--- 8-loops/loops.py
+++ 8-loops/loops.py
@@ -6,7 +6,6 @@ print("The rules are simple. I will think of a number, and you will try to guess
 number = random.randint(1, 10)
 isGuessRight = False
 
-# This is a comment.
 while isGuessRight != True:
     guess = input("Guess a number between 1 and 10: ")
     if int(guess) == number:
